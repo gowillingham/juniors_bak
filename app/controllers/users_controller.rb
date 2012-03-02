b48b431639ffdcb7b29253d13023c59d9298b8af
Stephen Willingham <gowillingham@gmail.com>
2012-03-02 10:11:39 -0600

    Refactor flash to display. Wire up edit, update, and delete actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
   
   def destroy
-    
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "The person #{@user.email} was removed"
+    redirect_to users_path
   end 
   
   def edit
-    
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+    if @user.save
+      flash[:success] = "Changes to person #{@user.email} were saved"
+      redirect_to users_path
+    else
+      render 'edit'
+    end
+  end 
   
   def new
     @user = User.new
@@ -15,7 +29,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to users_path, :success => 'Successfully added person to account. '
+      flash[:success] = "Added person #{@user.email} to your account"
+      redirect_to users_path
     else
       render 'new'
     end
