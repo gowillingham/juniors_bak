ea80fec7a7c84db754842a4f60f2c018cd6a4fa9
Stephen Willingham <gowillingham@gmail.com>
2012-03-04 08:04:26 -0600

    Write and pass validation tests for model registration.

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,2 +1,21 @@
 class Registration < ActiveRecord::Base
+  validates_presence_of :session, :message => ": You didn't select an inhouse session"
+
+  validates_presence_of :first_name, :last_name, :parent_first_name, :parent_last_name
+  validates_presence_of :address, :city, :state
+
+  validates_presence_of :zip  
+  validates_format_of :zip, :with => %r{\d{5}(-\d{4})?}
+
+  validates_presence_of :phone
+  validates_format_of :phone, :with => /^[\(\)0-9\- \+\.]{10,20}$/
+
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  validates_presence_of :email
+  validates_confirmation_of :email
+
+  validates_presence_of :school, :grade, :tshirt_size
+  validates_presence_of :parent_tshirt_size, :if => :parent_helper?, :message => "needed for parent helpers"
+  validates_presence_of :has_release, :message => ": Please read and accept the liability waiver"
+  validates :grade, :numericality => true
 end
--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -1,5 +1,75 @@
 require 'spec_helper'
 
 describe Registration do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+      :first_name => 'player',
+      :last_name => 'name', 
+      :parent_first_name => 'parent', 
+      :parent_last_name => 'name',
+      :grade => 3, 
+      :school => 'LME',
+      :email => 'player@gmail.com',
+      :phone => '9524316344', 
+      :address => '222 Inspiration path',
+      :city => 'Lake City',
+      :state => 'MN', 
+      :zip => '55044',
+      :parent_helper => true,
+      :tshirt_size => 'YL', 
+      :parent_tshirt_size => 'XL',
+      :note => "Some text that I'm sending along with this message as a note. Some text that I'm sending along with this message as a note. Some text that I'm sending along with this message as a note. ",
+      :session => 1,
+      :has_release => true
+    }
+  end
+  
+  it "should add a registration given valid attributes" do
+    lambda do
+      Registration.create!(@attr)
+    end.should change(Registration, :count).by(1) 
+  end 
+  
+  describe "validations" do 
+    it "should require a player first and last name" do
+      Registration.new(@attr.merge(:first_name => '')).should_not be_valid
+      Registration.new(@attr.merge(:last_name => '')).should_not be_valid
+    end
+    
+    it "should require a parent first and last name" do
+      Registration.new(@attr.merge(:parent_first_name => '')).should_not be_valid
+      Registration.new(@attr.merge(:parent_last_name => '')).should_not be_valid
+    end
+    
+    it "should require a valid email address" do
+      Registration.new(@attr.merge(:email => '')).should_not be_valid
+      Registration.new(@attr.merge(:email => 'email')).should_not be_valid
+    end
+    
+    it "shoud require a street address" do
+      Registration.new(@attr.merge(:address => '')).should_not be_valid
+      Registration.new(@attr.merge(:city => '')).should_not be_valid
+      Registration.new(@attr.merge(:state => '')).should_not be_valid
+      Registration.new(@attr.merge(:zip => '')).should_not be_valid
+    end
+    
+    it "should require a valid phone number" do 
+      Registration.new(@attr.merge(:phone => '')).should_not be_valid
+      Registration.new(@attr.merge(:phone => '999')).should_not be_valid
+      Registration.new(@attr.merge(:phone => 'x78-898-9999')).should_not be_valid
+    end
+    
+    it "should require a numeric grade" do
+      Registration.new(@attr.merge(:grade => nil)).should_not be_valid
+      Registration.new(@attr.merge(:grade => 'x')).should_not be_valid
+    end
+    
+    it "should require a tshirt size" do
+      Registration.new(@attr.merge(:tshirt_size => '')).should_not be_valid
+    end
+    
+    it "should require a session" do
+      Registration.new(@attr.merge(:session => nil)).should_not be_valid
+    end
+  end
 end
