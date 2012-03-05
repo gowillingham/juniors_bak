6bc481a134c8e9b076fd8a86faa348a96cfa4a1b
Stephen Willingham <gowillingham@gmail.com>
2012-03-05 13:26:15 -0600

    Move select options to globals initializer. Wire up selects to be part of the form.

--- config/initializers/globals.rb
+++ config/initializers/globals.rb
@@ -15,13 +15,21 @@ SCHOOL_OPTIONS = [
 ]
 
 SIZE_OPTIONS = [
-  ['Youth small', 'YS'], 
-  ['Youth medium', 'YM'], 
-  ['Youth large', 'YL'], 
-  ['Adult small', 'S'], 
-  ['Adult medium', 'M'], 
-  ['Adult large', 'L'], 
-  ['Adult X-large', 'XL'], 
-  ['Adult XX-large', 'XXL']
+  ['YS - Youth small', 'YS'], 
+  ['YM - Youth medium', 'YM'], 
+  ['YL - Youth large', 'YL'], 
+  ['S - Adult small', 'S'], 
+  ['M - Adult medium', 'M'], 
+  ['L - Adult large', 'L'], 
+  ['XL - Adult X-large', 'XL'], 
+  ['XXL - Adult XX-large', 'XXL']
 ]
+
+SESSION_OPTIONS = [
+  ['Session I Beginner (grade 1-3)', 1], 
+  ['Session II Intermediate (grade 3-6)', 2]
+]
+
+GRADE_OPTIONS = (1..6).map { |i| [i,i] }  
+
 
