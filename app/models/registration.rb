641970be38c2fbf39e70f7aa0f6ce670cab3523b
Stephen Willingham <gowillingham@gmail.com>
2012-03-20 08:45:30 -0500

    Allowed blank zip.

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -7,7 +7,7 @@ class Registration < ActiveRecord::Base
   validates :last_name, :presence => true
   validates :parent_first_name, :presence => true
   validates :parent_last_name, :presence => true
-  validates :zip, :format => { :with => %r{\d{5}(-\d{4})?} }
+  validates :zip, :allow_blank => true, :format => { :with => %r{\d{5}(-\d{4})?} }
 
   validates :school, :presence => true
   validates :grade, 
